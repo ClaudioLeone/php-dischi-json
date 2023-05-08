2b1d8c270623671d4f204aa8432a5ce7e3b76ea2
ClaudioLeone <claleone92@gmail.com>
2023-05-08 15:14:22 +0200

    Created basic docs, mounted Vue

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+const { createApp } = Vue;
+
+createApp ({
+    data() {
+        return {
+            testText: "TEST"
+        }
+    }
+}).mount("#app");
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Spotifake</title>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+    <div id="app">
+        <h1>{{ testText }}</h1>
+    </div>
+
+    <script src="JS/script.js"></script>
+</body>
+</html>
